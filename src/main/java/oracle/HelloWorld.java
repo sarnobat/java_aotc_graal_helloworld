39d7a9a65cdae7f617099cfd0ccef1e9c4de47f7
Sridhar Sarnobat <ss401533@gmail.com>
2020-07-31 21:46:32 -0700

    Minor changes (untested)

--- src/main/java/oracle/HelloWorld.java
+++ src/main/java/oracle/HelloWorld.java
@@ -1,5 +1,15 @@
 package oracle;
 
+import com.google.common.base.Function;
+import com.google.common.base.Preconditions;
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.ImmutableSet.Builder;
+import com.google.common.collect.Multimap;
+import com.google.common.util.concurrent.SimpleTimeLimiter;
+
 /**
  * OUr App. This will count the number of files within a directory, and sub dirs,
  * and also calculate the total size of all o fthe files.
@@ -11,6 +21,8 @@ public final class HelloWorld {
      * @param args The arguments of the program.
      */
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello Sridhar");
+    			ImmutableMap.Builder<String, Object> theParams = ImmutableMap.<String, Object>builder();
+
+        System.out.println("Hello " + theParams.toString());
     }
 }
