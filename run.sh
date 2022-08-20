53aababee6372787d5a80647d9fcc8fc7dc6105d
Sridhar Sarnobat <ss401533@gmail.com>
2022-08-19 21:27:06 -0700

    Minor changes (untested)

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
+alias native-image=/Library/Java/JavaVirtualMachines/graalvm-ce-java11-20.1.0/Contents/Home/lib/svm/bin/native-image
+
 rm helloworld
 mvn clean package -PJAVA
 native-image -jar ./target/graalvmnidemos-1.0-SNAPSHOT-jar-with-dependencies.jar --no-fallback --no-server -H:Class=oracle.HelloWorld -H:Name=helloworld
